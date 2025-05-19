6a15ae2d2d52f51d7acde42f5994188b91153b33
Mantas Kukulskis <8mantas8@gmail.com>
2025-05-19 11:14:28 +0300

    changed html & css files. js files structure

---
+++ js/board.js
@@ -0,0 +1,15 @@
+export function createBoard(boardElem, cellsArray, clickHandler) {
+  for (let i = 0; i < 100; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.dataset.index = i;
+    cell.addEventListener("click", () => clickHandler(i));
+    boardElem.appendChild(cell);
+    cellsArray.push(cell);
+  }
+}
+
+export function updateStatusText(text) {
+  const statusText = document.getElementById("status");
+  statusText.textContent = text;
+}
